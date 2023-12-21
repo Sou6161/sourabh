aa67110e78ccb242ca2f1ef6420b0c1f94088933
Sourabh Saini <sourabh61saini@gmail.com>
2023-12-21 12:39:17 +0530

    changes done

--- countdown.js
+++ countdown.js
@@ -1,3 +1,4 @@
+//countdown code starts from here
 let timer;
 let totalsec;
 let tension;
@@ -63,5 +64,75 @@ function resumecountdown() {
   document.getElementById("resumebutton").disabled = true;
 }
 
+//calender code starts from here
+let currentyear, currentmonth;
+function displaycalendar(year, month) {
+  let daysinmonth = new Date(year, month + 1, 0).getDate();
+  let firstday = new Date(year, month, 1).getDay();
+  let day = 1;
+  let calendarHTML = "";
 
+  let weeksinmonth = Math.ceil((daysinmonth + firstday) / 7);
+  for (let i = 1; i <= weeksinmonth; i++) {
+    calendarHTML += "<tr>";
 
+    for (let j = 1; j <= 7; j++) {
+      console.log(firstday, "firstdayyyy");
+      console.log(day, "dayyyy");
+      console.log(daysinmonth, "daysinmonth");
+      if ((i == 1 && j <= firstday) || day > daysinmonth) {
+        calendarHTML += "<td></td>";
+      } else {
+        calendarHTML += `<td>${day}</td>`;
+        day++;
+      }
+    }
+    calendarHTML += "</tr>";
+  }
+  console.log(calendarHTML, "calenderrrrrrrrrrr");
+  document.getElementById("calendar-body").innerHTML = calendarHTML;
+
+  let monthyearname = document.getElementById("month-year");
+  monthyearname.textContent = `${getmonthname(month)} ${year}`;
+}
+function prevMonth() {
+  currentmonth--;
+  if (currentmonth < 0) {
+    currentmonth = 11;
+    currentyear--;
+  }
+  displaycalendar(currentyear, currentmonth);
+}
+
+function nextMonth() {
+  currentmonth++;
+  if (currentmonth > 11) {
+    currentmonth = 0;
+    currentyear++;
+  }
+  displaycalendar(currentyear, currentmonth);
+}
+
+function getmonthname(monthindex) {
+  let months = [
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  return months[monthindex];
+}
+
+let today = new Date();
+currentyear = today.getFullYear();
+currentmonth = today.getMonth();
+
+displaycalendar(currentyear, currentmonth);
